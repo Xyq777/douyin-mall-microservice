c7c8cfea93d3470aa9ccd72a3f1662863fc144c2
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2025-03-05 14:22:39 +0800

    feat: 从环境变量读取所有配置

--- app/order/core/config.go
+++ app/order/core/config.go
@@ -1,32 +1,22 @@
 package core
 
 import (
-	"fmt"
 	"github.com/All-Done-Right/douyin-mall-microservice/app/order/global"
 	"github.com/Mmx233/EnvConfig"
 	"github.com/joho/godotenv"
-	"github.com/pelletier/go-toml"
-	"log"
-	"os"
+	"github.com/sirupsen/logrus"
 )
 
 func InitConfig() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-	configPath := "conf/%s/config.toml"
-	configPath = fmt.Sprintf(configPath, "test")
-	if mode := os.Getenv("ENV"); mode != "" {
-		configPath = fmt.Sprintf(configPath, mode)
-	}
-	tree, err := toml.LoadFile(configPath)
-	if err != nil {
-		log.Fatalf("Error loading TOML file: %v", err)
-	}
-	err = tree.Unmarshal(&global.Config)
-	if err != nil {
-		log.Fatalf("Error unmarshalling TOML into struct: %v", err)
+		logrus.Error(err)
+		return
 	}
 	EnvConfig.Load("ORDER_SERVICE_", &global.Config.ServiceInfo)
+	EnvConfig.Load("ORDER_CONSUL_", &global.Config.Consul)
+	EnvConfig.Load("ORDER_MYSQL_", &global.Config.Mysql)
+	EnvConfig.Load("ORDER_REDIS_", &global.Config.Redis)
+	EnvConfig.Load("ORDER_LOGGER_", &global.Config.Logger)
+
 }
